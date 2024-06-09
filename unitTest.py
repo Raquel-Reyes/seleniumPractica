496655c08adc9824db93f7820bd88c8743ccd333
SoIsaac7 <150219669+SoIsaac7@users.noreply.github.com>
2024-06-09 00:25:35 -0600

    Cambios en unitTest
    Se realizaron cambios para el archivo de prueba.

--- unitTest.py
+++ unitTest.py
@@ -13,39 +13,19 @@ class PythonOrgSearch(unittest.TestCase):
         driver = self.driver
 
         #Script de Danilo Bulux
-        import unittest
-        from selenium import webdriver
-        from selenium.webdriver.common.keys import Keys
-        from selenium.webdriver.common.by import By
-        import time
-
-        class TryTestingThisSite(unittest.TestCase):
-            def setUp(self):
-                self.driver = webdriver.Chrome()
-
-            def test_interact_with_try_testing_site(self):
-                driver = self.driver
-                driver.get("https://trytestingthis.netlify.app/")
-                driver.set_window_size(1532, 816)
-                # Interactuar con los elementos de la página
-                #driver.find_element(By.ID, "fname").click()
-                var = driver.find_element(By.ID, "fname")
-                var.send_keys("Danilo")
-                driver.find_element(By.ID, "lname").click()
-                driver.find_element(By.ID, "lname").send_keys("Bulux")
-                driver.find_element(By.ID, "male").click()
-                driver.find_element(By.ID, "option").click()
-                dropdown = driver.find_element(By.ID, "option")
-                dropdown.find_element(By.XPATH, "//option[. = 'Option 1']").click()
-                dropdown = driver.find_element(By.ID, "owc")
-                dropdown.find_element(By.XPATH, "//option[. = 'Option 1']").click()
-                driver.find_element(By.ID, "moption").click()
-                driver.find_element(By.ID, "day").click()
-                driver.find_element(By.ID, "day").send_keys("07-06-2024")
-                time.sleep(10)
-
-        if __name__ == "__main__":
-            unittest.main()
+        driver.get("https://trytestingthis.netlify.app/")
+        driver.set_window_size(1532, 816)
+        # Interactuar con los elementos de la página
+        driver.find_element(By.ID, "fname").send_keys("Danilo")
+        driver.find_element(By.ID, "lname").send_keys("Bulux")
+        driver.find_element(By.ID, "male").click()
+        dropdown = driver.find_element(By.ID, "option")
+        dropdown.find_element(By.XPATH, "//option[. = 'Option 1']").click()
+        dropdown = driver.find_element(By.ID, "owc")
+        dropdown.find_element(By.XPATH, "//option[. = 'Option 1']").click()
+        driver.find_element(By.ID, "moption").click()
+        driver.find_element(By.ID, "day").send_keys("07-06-2024")
+        time.sleep(5)
 
         #Script de Iris González
                 
@@ -71,7 +51,6 @@ class PythonOrgSearch(unittest.TestCase):
         #Script de Isaac Tuiz
         # Selecciono mi color favorito
         driver.find_element(By.ID, "favcolor").send_keys("#008000")
-
         # Selecciono una cantidad
         driver.find_element(By.ID, "quantity").clear()
         driver.find_element(By.ID, "quantity").send_keys("10")
